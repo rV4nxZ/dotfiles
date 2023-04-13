87578359c13e191943550ee66b85628322a39a4d
Joan Contreras <75918520+rV4nxZ@users.noreply.github.com>
2023-04-13 17:51:04 -0400

    Add files via upload

---
+++ config/zsh/aliases.zsh
@@ -0,0 +1,15 @@
+##
+## Aliases
+##
+
+alias ls="exa --color=auto --icons --group-directories-first -a"
+alias l="ls -l"
+alias lla="ls -la"
+alias lt="ls --tree"
+alias cat="bat --color always --plain"
+alias grep='grep --color=auto'
+alias mv='mv -v'
+alias cp='cp -vr'
+alias rm='rm -vr'
+
+# vim:ft=zsh
